43c26a612e6d0a5944a5abc23d0a5931bab6745e
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-12-04 22:25:45 -0500

    fixed issue in reaction test where users were able to continue the test after the time expired

--- public/js/reaction.js
+++ public/js/reaction.js
@@ -1,21 +1,23 @@
-  // Player clicks start button
-  document.querySelector('#startGame').addEventListener('click', startGame)
-  document.querySelector('#startGame').addEventListener('click', targetFlip)
-  
-    // Trigger start of game
-  // Timer starts, counting down from 60sec to 0
-  let countdown = 60
-  let highScoreValue = 0
-  function startGame(){
-    const scoreElement = document.querySelector('#count');
-    const interval = setInterval(() => {
+// Player clicks start button
+document.querySelector('#startGame').addEventListener('click', startGame)
+document.querySelector('#startGame').addEventListener('click', targetFlip)
+const highScore = document.querySelector('#highScore');
+
+// Trigger start of game
+// Timer starts, counting down from 60sec to 0
+let countdown = 60
+let highScoreValue = parseInt(highScore.innerText)
+function startGame() {
+  countdown = 60
+  const scoreElement = document.querySelector('#count');
+  const interval = setInterval(() => {
     document.querySelector('.timer').innerHTML = 'Time Left: <span id="clock">`${countdown}`</span>'
     document.querySelector('#startGame').style.display = 'none'
     document.querySelector('.timer').style.color = 'inherit'
     document.querySelector('#clock').innerText = countdown
-    
-    
+
     countdown--;
+
     if (countdown < -1) {
       // Stop the interval
       clearInterval(interval);
@@ -26,62 +28,70 @@
       addNewReactionGameScore(scoreElement)
       updateHighScore(highScoreValue)
       scoreElement.innerText = 0
-      countdown = 60
-    } 
-    
-  }, 1000);
-  
-  }
-  
-  function targetFlip() {
-    const tarNum = Math.floor(Math.random() * 25) + 1;
-    const scoreElement = document.querySelector('#count');
-    const highScore = document.querySelector('#highScore');
-    if (countdown > -1) {
-      // Reset the color of all target elements
-      document.querySelectorAll('.target').forEach(element => {
-        element.style.background = 'radial-gradient(circle, #0D446C, #167DC6)';
-        // Remove click event listener from all elements
-        element.removeEventListener('click', handleClick);
-      });
-  
-      // Set the color of the random target to red
-      document.querySelector(`.tar${tarNum}`).style.background = 'red';
-      document.querySelector(`.tar${tarNum}`).classList.add('flipped');
-    
-      
-      // Add a click event listener to the red target element
-      document.querySelector(`.tar${tarNum}`).addEventListener('click', handleClick);
-      document.querySelector(`.tar${tarNum}`).addEventListener('animationend', function() {
-        this.classList.remove('flipped');
-      }, {once: true});
-    } 
-  
-    function handleClick() {
-      // Increment the score
-      let score = parseInt(scoreElement.innerText) + 1;
-      scoreElement.innerText = score;
-  
-      // Update the high score if the current score exceeds the high score
-      if (score > highScoreValue) {
-        highScoreValue = score;
-        highScore.innerText = highScoreValue;
-      }
-      document.querySelector(`.tar${tarNum}`).removeEventListener('click', handleClick);
-      
-      // Call targetFlip again to show a new red target
-      targetFlip();
     }
+
+  }, 1000);
+
+}
+
+function targetFlip() {
+  const tarNum = Math.floor(Math.random() * 25) + 1;
+  const scoreElement = document.querySelector('#count');
+
+  function handleClick() {
+    let score = parseInt(scoreElement.innerText) + 1;
+    if(countdown > 0) {
+      // Increment the score
+      scoreElement.innerText = score;
+    }
+
+    // Update the high score if the current score exceeds the high score
+    if (score > highScoreValue) {
+      highScoreValue = score;
+      highScore.innerText = highScoreValue;
+    }
+    document.querySelector(`.tar${tarNum}`).removeEventListener('click', handleClick);
+
+    // Call targetFlip again to show a new red target
+    targetFlip();
+
   }
 
-  function addNewReactionGameScore(scoreElement) {
-    fetch('/tests/addNewReactionGameScore', {
-      method: 'put',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        newScore: parseFloat(scoreElement.innerText)
-      })
+  if (countdown > 0) {
+    // Reset the color of all target elements
+    document.querySelectorAll('.target').forEach(element => {
+      element.style.background = 'radial-gradient(circle, #0D446C, #167DC6)';
+      // Remove click event listener from all elements
+      element.removeEventListener('click', handleClick);
+    });
+
+    // Set the color of the random target to red
+    document.querySelector(`.tar${tarNum}`).style.background = 'red';
+    document.querySelector(`.tar${tarNum}`).classList.add('flipped');
+
+
+    // Add a click event listener to the red target element
+    document.querySelector(`.tar${tarNum}`).addEventListener('click', handleClick);
+    document.querySelector(`.tar${tarNum}`).addEventListener('animationend', function () {
+      this.classList.remove('flipped');
+    }, { once: true });
+   
+  }else {
+    document.querySelectorAll('.target').forEach(element => {
+      element.style.background = 'radial-gradient(circle, #0D446C, #167DC6)';
+      element.removeEventListener('click', handleClick);
+    });
+  }
+}
+
+function addNewReactionGameScore(scoreElement) {
+  fetch('/tests/addNewReactionGameScore', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      newScore: parseFloat(scoreElement.innerText)
     })
+  })
     .then(response => {
       if (response.ok) return response.json()
     })
@@ -89,16 +99,16 @@
       console.log(data)
       window.location.reload(true)
     })
-  }
+}
 
-  function updateHighScore(highScoreValue) {
-    fetch('/tests/updateReactionTestHighScore', {
-      method: 'put',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        highScore: parseFloat(highScoreValue)
-      })
+function updateHighScore(highScoreValue) {
+  fetch('/tests/updateReactionTestHighScore', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      highScore: parseFloat(highScoreValue)
     })
+  })
     .then(response => {
       if (response.ok) return response.json()
     })
@@ -106,5 +116,5 @@
       console.log(data)
       window.location.reload(true)
     })
-  }
+}
 
---
+++ public/js/recall.js
@@ -0,0 +1,15 @@
+//this will be an active recall test
+//while loop that runs until user solution is wrong
+//display shapes to user, then prompt them to recall what shape it is
+//each level of test is array of shapes (easy: 3 shape, medium: 4 shape: hard: 5 shape)
+
+//original array before randomized
+['triangle', 'square', 'circle']
+
+//if user answers match in correct order, move on to next round
+['square', 'circle', 'triangle']
+
+
+//have a list of clickable shapes
+//if they click a shape, push that shape to answer array
+//innerHTML of that element would now be everything in the answer array
