c0a41ec6866bc301aaa51c36786134af2041a1e6
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-12-08 19:44:14 -0500

    updated to ensure that numberSequence.ejs loads in

--- controllers/numberSequenceTests.js
+++ controllers/numberSequenceTests.js
@@ -1,30 +1,35 @@
 const NumberSequenceTest = require("../models/NumberSequenceTest");
 
-getNumberSequenceTest: async (req, res) => {
-    try {
-      let test = await RecallTest.find({ user: req.user.id });
-      if (test.length) {
-        res.render("numberSequence.ejs", { user: req.user, scores: test });
-      } else {
-        const numberSequenceTest = new NumberSequenceTest({
-          user: req.user.id,
-          easyScores: [],
-          easyHighScore: 0,
-          mediumScores: [],
-          mediumHighScore: 0,
-          hardScores: [],
-          hardHighScore: 0,
-          expertScores: [],
-          expertHighScore: 0,
-          impossibleScores: [],
-          impossibleHighScore: 0
-        });
+module.exports = {
 
-        await numberSequenceTest.save();
-        test = await NumberSequenceTest.find({ user: req.user.id });
-        res.render("numberSequence.ejs", { user: req.user, scores: test });
+    getNumberSequenceTest: async (req, res) => {
+        try {
+          let test = await NumberSequenceTest.find({ user: req.user.id });
+          if (test.length) {
+            res.render("numberSequence.ejs", { user: req.user, scores: test });
+          } else {
+            const numberSequenceTest = new NumberSequenceTest({
+              user: req.user.id,
+              easyScores: [],
+              easyHighScore: 0,
+              mediumScores: [],
+              mediumHighScore: 0,
+              hardScores: [],
+              hardHighScore: 0,
+              expertScores: [],
+              expertHighScore: 0,
+              impossibleScores: [],
+              impossibleHighScore: 0
+            });
+    
+            await numberSequenceTest.save();
+            test = await NumberSequenceTest.find({ user: req.user.id });
+            res.render("numberSequence.ejs", { user: req.user, scores: test });
+          }
+        } catch (err) {
+          console.log(err);
+        }
       }
-    } catch (err) {
-      console.log(err);
-    }
-  }
+
+}
+
